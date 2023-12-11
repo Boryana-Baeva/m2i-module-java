193d0f37036cf83211abe78a7bacd6069bb37061
Boryana Baeva <b.baeva@labom2iformation.fr>
2023-12-11 11:06:26 +0100

    Point 3:  Affichage avec concatenation

---
+++ src/Point3Affichage.java
@@ -0,0 +1,11 @@
+public class Point3Affichage {
+    public static void main(String[] args) {
+        String firstName = "Christian";
+        String lastName = "Lisangola";
+        int age = 17;
+        double weight = 35.4;
+
+        // Je m'appelle Christian Lisangola, j'ai 17 ans et je pèse 35.4 Kg.
+        System.out.println("Je m'appelle " + firstName + " " + lastName + ", j'ai " + age + " ans et je pèse " + weight + " Kg.");
+    }
+}
