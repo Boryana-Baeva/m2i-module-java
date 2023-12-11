bd6b4462ae33ff2cebfda166cafa6af2fa3430bf
Boryana Baeva <b.baeva@labom2iformation.fr>
2023-12-11 10:34:27 +0100

    Point2TypeText

---
+++ src/Point2TypeText.java
@@ -0,0 +1,12 @@
+public class Point2TypeText {
+    public static void main(String[] args) {
+        String firstName = "Boryana";
+        String lastName = "Rault";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        char gender = 'F';
+        System.out.println(gender);
+    }
+}
